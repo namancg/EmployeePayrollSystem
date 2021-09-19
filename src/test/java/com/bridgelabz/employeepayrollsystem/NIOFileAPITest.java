7ca83ac4521ce420835c4d74c2d2d6324de2d5f0
namancg <naman.c@perfios.com>
2021-09-19 17:34:13 +0530

    ADDED: tested watch service for a directory

--- src/test/java/com/bridgelabz/employeepayrollsystem/NIOFileAPITest.java
+++ src/test/java/com/bridgelabz/employeepayrollsystem/NIOFileAPITest.java
@@ -44,10 +44,14 @@ public class NIOFileAPITest
 		// List Files, Directories as well as Files with Extension
 		Files.list(playPath).filter(Files::isRegularFile)
 				.forEach(System.out::println);
-
 		Files.newDirectoryStream(playPath).forEach(System.out::println);
-
 		Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp"))
 				.forEach(System.out::println);
+	}
+	@Test
+	public void givenADirectoryWhenWatchedListsAllTheActivities() throws IOException {
+		Path dir = Paths.get( HOME+ "/"+PLAY_WITH_NIO) ;
+		Files. list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+		new Java8WatchServiceExample(dir).processEvents();
 	}
 }

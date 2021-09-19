42f20ccd4792fb6989c043a4cf9fcf6e1caeef59
namancg <naman.c@perfios.com>
2021-09-19 17:59:15 +0530

    ADDED: test method to write into a file

---
+++ src/test/java/com/bridgelabz/employeepayrollsystem/EmployeePayrollServiceTest.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.employeepayrollsystem;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+public class EmployeePayrollServiceTest {
+
+	@Test
+	public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEntries() {
+		EmployeePayrollData[] arrayOfEmps = {
+				new EmployeePayrollData( 1, "Jeff Bezos", 100000.0),
+				new EmployeePayrollData( 2, "Bill Gates",  200000.0),
+				new EmployeePayrollData( 3, "Mark Zuckerberg", 300000.0)
+		};
+		EmployeePayrollService employeePayrollService;
+		employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		employeePayrollService.writeEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+	}
+
+}

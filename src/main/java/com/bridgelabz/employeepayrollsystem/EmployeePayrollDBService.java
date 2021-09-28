0fc4770a48c517f32e8548ed48f06cb7a4fd459f
namancg <naman.c@perfios.com>
2021-09-28 20:07:39 +0530

    REFACTORED: logic to add new emloyee to payroll

--- src/main/java/com/bridgelabz/employeepayrollsystem/EmployeePayrollDBService.java
+++ src/main/java/com/bridgelabz/employeepayrollsystem/EmployeePayrollDBService.java
@@ -9,6 +9,7 @@ import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.Date;
 
 import com.bridgelabz.employeepayrollsystem.EmployeePayrollException.ExceptionType;	
 
@@ -275,6 +276,30 @@ public class EmployeePayrollDBService {
 			e.printStackTrace();
 		}
 		return CountBasedOnGender;
+	}
+public EmployeePayrollData addEmployeeToPayroll(String name, double salary, LocalDate startDate, String gender) {
+		
+		int employeeId = -1;
+		EmployeePayrollData employeePayrollData = null;
+		String sql = String.format("INSERT INTO employee_payroll (name, gender, salary, startDate) VALUES ('%s', '%s', '%s', '%s')", name, gender, salary, Date.valueOf(startDate));
+		
+		try (Connection connection = this.getConnection()){
+			
+			Statement statement = connection.createStatement();
+			int rowAffected = statement.executeUpdate(sql, statement.RETURN_GENERATED_KEYS);
+			if(rowAffected == 1) {
+				ResultSet resultSet = statement.getGeneratedKeys();
+				if(resultSet.next())
+					employeeId = resultSet.getInt(1);
+			}
+			employeePayrollData = new EmployeePayrollData(employeeId, name, salary, startDate);
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return employeePayrollData;
+		
 	}
 	
 	
